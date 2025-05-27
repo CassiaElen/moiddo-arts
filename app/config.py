9d3c36dda3d22eb6d96afa45359165317c6709ec
Gabriel Sidrim <gsidrimdiniz@gmail.com>
2025-05-27 15:10:34 -0300

    Update config.py
    Corrigindo um pequeno erro no caminho do BD

--- app/config.py
+++ app/config.py
@@ -4,5 +4,5 @@ import secrets
 class Config:
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))  # Define o diretório base do projeto
     SECRET_KEY = secrets.token_hex(16)  # Gera uma chave secreta aleatória para a aplicação
-    DATABASE = os.path.join(BASE_DIR, "moiddo-arts.db")   # Define a URI do banco de dados SQLite
+    DATABASE = os.path.join(BASE_DIR, "moiddo_arts.db")   # Define a URI do banco de dados SQLite
     
