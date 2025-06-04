3056d16f0441419ad28da38e46e9c32696f338c5
gabriel sidrim <gsidrimdiniz@gmail.com>
2025-06-04 13:15:14 -0300

    incrementações no init e routes,além de uma pequena correção em um template

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from app.config import Config
 import sqlite3
 import secrets
 import os
@@ -7,6 +8,7 @@ import os
 def create_app():
     app = Flask(__name__,template_folder='templates',static_folder='static')
     app.config.from_pyfile(os.path.join(app.root_path, 'config.py')) # Adicionar um arquivo externo para carregar as configurações do projeto.
+    app.config.from_object(Config)  # aplica as configurações da classe
     
     from app.routes import main
     app.register_blueprint(main)
--- app/routes.py
+++ app/routes.py
@@ -6,13 +6,13 @@ from datetime import datetime
 main = Blueprint('main', __name__)
 
 #Função para cadastrar cliente:
-def RegisterClient(email,nome_completo,senha,cpf,usuario): # Adicionar o nome de usuário e o cpf
+def RegisterClient(nome_completo,usuario,email,cpf,senha): 
     conn = sqlite3.connect("moiddo_arts.db")
     cursor = conn.cursor()
 
     try:
-        data_cadastro = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        cursor.execute("INSERT INTO comprador (email,nome_completo,senha,cpf,usuario,data_cadastro) VALUES (?,?,?,?,?,?)", (email,nome_completo,senha,cpf,usuario,data_cadastro))
+        data_cadastro = datetime.now().strftime("%Y-%m-%d")
+        cursor.execute("INSERT INTO cliente (nome_completo,usuario,email,cpf,senha) VALUES (?,?,?,?,?)", (nome_completo,usuario,email,cpf,senha))
         conn.commit()
     
     except sqlite3.IntegrityError:
@@ -23,13 +23,13 @@ def RegisterClient(email,nome_completo,senha,cpf,usuario): # Adicionar o nome de
     return True
 
 #Função para cadastrar Artista:
-def RegisterArtist(email, nome_completo, senha, cpf_cnpj,usuario): # Adicionar o nome de usuário
+def RegisterArtist(nome_completo,usuario,email,cpf_cnpj,senha): # Adicionar o nome de usuário
     conn = sqlite3.connect("moiddo_arts.db")
     cursor = conn.cursor()
 
     try:
-        data_cadastro = datetime.datetime.now().strftime("%Y-%m-%d")
-        cursor.execute("INSERT INTO artistas (email,nome_completo,senha,cpf_cnpj,usuario,data_cadastro) VALUES (?,?,?,?,?,?)", (email,nome_completo,senha,cpf_cnpj,usuario,data_cadastro))
+        data_cadastro = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        cursor.execute("INSERT INTO artistas (nome_completo,usuario,email,cpf_cnpj,senha) VALUES (?,?,?,?,?)", (nome_completo,usuario,email,cpf_cnpj,senha))
         conn.commit()
     
     except sqlite3.IntegrityError:
@@ -77,7 +77,7 @@ def login_client():
 
         if not email or not senha:
             flash("Todos os campos são obrigatórios!")
-            return(redirect(url_for("login_client")))
+            return(redirect(url_for("main.login_client")))
         
         user = CheckLoginClient(email, senha)
         if user:
@@ -85,7 +85,7 @@ def login_client():
             return redirect(url_for("home"))
         else:
             flash("Email ou senha incorretos!")
-            return redirect(url_for("login_client"))
+            return redirect(url_for("main.login_client"))
     return render_template("login-cliente.html")
 
 @main.route("/login/artista", methods=["GET","POST"])
@@ -96,59 +96,59 @@ def login_artist():
 
         if not email or not senha:
             flash("Todos os campos são obrigatórios!")
-            return redirect(url_for("login_artist"))
+            return redirect(url_for("main.login_artist"))
         
         user = CheckLoginArtist(email, senha)
         if user:
             flash("Login realizado com sucesso!")
-            return redirect(url_for("home"))
+            return redirect(url_for("main.home"))
         else:
             flash("Email ou senha incorretos!")
-            return redirect(url_for("login_artist"))
+            return redirect(url_for("main.login_artist"))
     return render_template("login-artesao.html")
 
 
 @main.route('/register/cliente', methods=['GET', 'POST'])
 def register_client():
     if request.method == "POST":
-        email = request.form.get("email")
-        nome_completo = request.form.get("name")
-        senha = request.form.get("password")
-        cpf = request.form.get("cpf")  
-        usuario = request.form.get("user")
+        nome_completo = request.form["name"]
+        usuario = request.form["user"]
+        email = request.form["email"]
+        cpf = request.form["cpf"]         
+        senha = request.form["password"]
 
         if not email or not nome_completo or not senha or not cpf or not usuario:
             flash("Todos os campos são obrigatórios!")
-            return redirect(url_for("register_client"))
+            return redirect(url_for("main.register_client"))
         
-        if RegisterClient(email,nome_completo,senha,cpf,usuario): 
+        if RegisterClient(nome_completo,usuario,email,cpf,senha): 
             flash("Cadastro realizado com sucesso!")
             flash("Faça o seu login como Cliente!")
-            return redirect(url_for("login_client"))
+            return redirect(url_for("main.login_client"))
         else:
             flash("Usuário já cadastrado!")
-            return redirect(url_for("register_client"))
+            return redirect(url_for("main.register_client"))
     return render_template("cadastro-cliente.html")
 
 @main.route("/register/artista", methods=["GET","POST"])
 def register_artist():
     if request.method == "POST":
-        email = request.form["email"]
         nome_completo = request.form["name"]
-        senha = request.form["password"]
+        usuario = request.form["user"]
+        email = request.form["email"]
         cpf_cnpj = request.form["cpf"]
-        usuario = request.form["name"]
+        senha = request.form["password"]
 
         if not email or not nome_completo or not senha or not cpf_cnpj or not usuario: 
             flash("Todos os campos são obrigatórios!")
-            return redirect(url_for("register_artist"))
+            return redirect(url_for("main.register_artist"))
         
-        if RegisterArtist(email, nome_completo, senha, cpf_cnpj,usuario):
+        if RegisterArtist(nome_completo,usuario,email,cpf_cnpj,senha):
             flash("Cadastro realizado com sucesso!")
             flash("Faça o seu login com Artesão!")
-            return redirect(url_for("login_artist"))
+            return redirect(url_for("main.login_artist"))
         else:
             flash("Usuário já cadastrado!")
-            return redirect(url_for("register_artist"))
+            return redirect(url_for("main.register_artist"))
         
     return render_template("cadastro-artesao.html")
