8c6bc3450017ef4c717be73ab51aa97039a87b66
Gabriel Sidrim <gsidrimdiniz@gmail.com>
2025-06-05 15:26:41 -0300

    Update config.py
    Só corrigi o detalhe do comentário

--- app/config.py
+++ app/config.py
@@ -5,4 +5,4 @@ class Config:
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))  # Define o diretório base do projeto
     SECRET_KEY = secrets.token_hex(16)  # Gera uma chave secreta aleatória para a aplicação
     DATABASE = os.path.join(BASE_DIR, "moiddo_arts.db")   # Define a URI do banco de dados SQLite
-    PERMANENT_SESSION_LIFETIME =  1800 # Define o tempo de vida da sessão permanente em segundos (3 minutos) 
+    PERMANENT_SESSION_LIFETIME =  1800 # Define o tempo de vida da sessão permanente em segundos (30 minutos) 
