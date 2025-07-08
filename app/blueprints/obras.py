66db1722735aa16b4fd93d0143900998da8081d3
matheus sales <156109892+Math-eus1@users.noreply.github.com>
2025-07-08 14:25:19 -0300

    adicionando o que é filtrado pelo formulário

--- app/blueprints/obras.py
+++ app/blueprints/obras.py
@@ -1,8 +1,11 @@
 from flask import Blueprint
+from flask import request
 
 obras_bp = Blueprint('obras', __name__)
 
 @obras_bp.route('/loja', methods = ['POST'])
+#inicialmente, capturar as informações enviadas via formulário pelo usuário
 def filtrar_categorias():
-    return ("oi")
+    filtro1 = request.form.getlist('pá')
+
     
